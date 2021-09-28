bfa9116bbe1831f5ec782e64d1e9eb3fbea6c276
Lala Muradova <Lalamuradova2017@gmail.com>
2021-09-28 15:18:30 +0400

    d

--- FunitureFactory/MainWindow.xaml.cs
+++ FunitureFactory/MainWindow.xaml.cs
@@ -18,227 +18,230 @@ namespace FunitureFactory
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    ///  interface IChair
-//    {
-//        bool sitOn();
-//    bool hasLegs();
-//}
-//class ArtDecoChair : IChair
-//{
-//    public bool hasLegs()
-//    {
-//        return true;
-//    }
-
-//    public bool sitOn()
-//    {
-//        return true;
-//    }
-//}
-//class ModernChair : IChair
-//{
-//    public bool hasLegs()
-//    {
-//        return true;
-//    }
-
-//    public bool sitOn()
-//    {
-//        return true;
-//    }
-//}
-//class VictorianChair : IChair
-//{
-//    public bool hasLegs()
-//    {
-//        return true;
-//    }
-
-//    public bool sitOn()
-//    {
-//        return false;
-//    }
-//}
-
-//interface ITable
-//{
-//    bool sitOn();
-//    bool hasLegs();
-//}
-
-//class ArtDecoTable : ITable
-//{
-//    public bool hasLegs()
-//    {
-//        return true;
-//    }
-
-//    public bool sitOn()
-//    {
-//        return false;
-//    }
-//}
-//class ModernTable : ITable
-//{
-//    public bool hasLegs()
-//    {
-//        return false;
-//    }
-
-//    public bool sitOn()
-//    {
-//        return true;
-//    }
-//}
-//class VictorianTable : ITable
-//{
-//    public bool hasLegs()
-//    {
-//        return false;
-//    }
-
-//    public bool sitOn()
-//    {
-//        return true;
-//    }
-//}
-
-//interface ISofa
-//{
-
-//    bool canEnlarge();
-//}
-
-//class ArtSofa : ISofa
-//{
-//    public bool canEnlarge()
-//    {
-//        return false;
-//    }
-
-
-//}
-//class ModernSofa : ISofa
-//{
-//    public bool canEnlarge()
-//    {
-//        return false;
-//    }
-
-//}
-//class VictorianSofa : ISofa
-//{
-//    public bool canEnlarge()
-//    {
-//        return true;
-//    }
-
-//}
-
-
-//interface IFurnitureFactory
-//{
-//    IChair CreateChar();
-//    ITable CreateTable();
-//    ISofa CreateSofa();
-//    void Show();
-//}
-
-//class ModernFunitureFactory : IFurnitureFactory
-//{
-//    public IChair CreateChar()
-//    {
-//        return new ModernChair();
-//    }
-
-//    public ISofa CreateSofa()
-//    {
-//        return new ModernSofa();
-//    }
-
-//    public ITable CreateTable()
-//    {
-//        return new ModernTable();
-//    }
-
-//    public void Show()
-//    {
-//        Console.WriteLine("Modern Style");
-//        var chair = CreateChar();
-//        var table = CreateTable();
-//        var sofa = CreateSofa();
-//    }
-
-//}
-
-//class VictorianFunitureFactory : IFurnitureFactory
-//{
-//    public IChair CreateChar()
-//    {
-//        return new VictorianChair();
-//    }
-
-//    public ISofa CreateSofa()
-//    {
-//        return new VictorianSofa();
-//    }
-
-//    public ITable CreateTable()
-//    {
-//        return new VictorianTable();
-//    }
-
-//    public void Show()
-//    {
-//        Console.WriteLine("Victorian Style");
-//        var chair = CreateChar();
-//        var table = CreateTable();
-//        var sofa = CreateSofa();
-//    }
-
-//}
-//class ArtDecoFunitureFactory : IFurnitureFactory
-//{
-//    public IChair CreateChar()
-//    {
-//        return new ArtDecoChair();
-//    }
-
-//    public ISofa CreateSofa()
-//    {
-//        return new ArtSofa();
-//    }
-
-//    public ITable CreateTable()
-//    {
-//        return new ArtDecoTable();
-//    }
-
-//    public void Show()
-//    {
-
-//        Console.WriteLine("ArtDeco Style");
-//        var chair = CreateChar();
-//        var table = CreateTable();
-//        var sofa = CreateSofa();
-//    }
-//}
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        //GUIFactory f = new WinFactory();
-//        //Application app = new Application(f);
-//        var funiturefactory = new ModernFunitureFactory();
-//        funiturefactory.Show();
-
-//    }
-//}
-public partial class MainWindow : Window
+ 
+    public partial class MainWindow : Window
     {
+        interface IChair
+        {
+            bool sitOn();
+            bool hasLegs();
+        }
+        class ArtDecoChair : IChair
+        {
+            public bool hasLegs()
+            {
+                return true;
+            }
+
+            public bool sitOn()
+            {
+                return true;
+            }
+        }
+        class ModernChair : IChair
+        {
+            public bool hasLegs()
+            {
+                return true;
+            }
+
+            public bool sitOn()
+            {
+                return true;
+            }
+        }
+        class VictorianChair : IChair
+        {
+            public bool hasLegs()
+            {
+                return true;
+            }
+
+            public bool sitOn()
+            {
+                return false;
+            }
+        }
+
+        interface ITable
+        {
+            bool sitOn();
+            bool hasLegs();
+        }
+
+        class ArtDecoTable : ITable
+        {
+            public bool hasLegs()
+            {
+                return true;
+            }
+
+            public bool sitOn()
+            {
+                return false;
+            }
+        }
+        class ModernTable : ITable
+        {
+            public bool hasLegs()
+            {
+                return false;
+            }
+
+            public bool sitOn()
+            {
+                return true;
+            }
+        }
+        class VictorianTable : ITable
+        {
+            public bool hasLegs()
+            {
+                return false;
+            }
+
+            public bool sitOn()
+            {
+                return true;
+            }
+        }
+
+        interface ISofa
+        {
+
+            bool canEnlarge();
+        }
+
+        class ArtSofa : ISofa
+        {
+            public bool canEnlarge()
+            {
+                return false;
+            }
+
+
+        }
+        class ModernSofa : ISofa
+        {
+            public bool canEnlarge()
+            {
+                return false;
+            }
+
+        }
+        class VictorianSofa : ISofa
+        {
+            public bool canEnlarge()
+            {
+                return true;
+            }
+
+        }
+
+
+        interface IFurnitureFactory
+        {
+            IChair CreateChar();
+            ITable CreateTable();
+            ISofa CreateSofa();
+            void Show();
+        }
+
+        class ModernFunitureFactory : IFurnitureFactory
+        {
+            public IChair CreateChar()
+            {
+                return new ModernChair();
+            }
+
+            public ISofa CreateSofa()
+            {
+                return new ModernSofa();
+            }
+
+            public ITable CreateTable()
+            {
+                return new ModernTable();
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("Modern Style");
+                var chair = CreateChar();
+                var table = CreateTable();
+                var sofa = CreateSofa();
+            }
+
+        }
+
+        class VictorianFunitureFactory : IFurnitureFactory
+        {
+            public IChair CreateChar()
+            {
+                return new VictorianChair();
+            }
+
+            public ISofa CreateSofa()
+            {
+                return new VictorianSofa();
+            }
+
+            public ITable CreateTable()
+            {
+                return new VictorianTable();
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("Victorian Style");
+                var chair = CreateChar();
+                var table = CreateTable();
+                var sofa = CreateSofa();
+            }
+
+        }
+        class ArtDecoFunitureFactory : IFurnitureFactory
+        {
+            public IChair CreateChar()
+            {
+                return new ArtDecoChair();
+            }
+
+            public ISofa CreateSofa()
+            {
+                return new ArtSofa();
+            }
+
+            public ITable CreateTable()
+            {
+                return new ArtDecoTable();
+            }
+
+            public void Show()
+            {
+
+                Console.WriteLine("ArtDeco Style");
+                var chair = CreateChar();
+                var table = CreateTable();
+                var sofa = CreateSofa();
+            }
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
         }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //GUIFactory f = new WinFactory();
+                //Application app = new Application(f);
+                var funiturefactory = new ModernFunitureFactory();
+                funiturefactory.Show();
+
+            }
+        }
+
     }
 }
